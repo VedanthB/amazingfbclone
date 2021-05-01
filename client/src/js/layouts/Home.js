460c0596e107bd584d0452d70e5a856c980ce590
Soham K <sohamk10@gmail.com>
2021-05-01 18:14:01 +0530

    Switched HashRouter for BrowserRouter. Updated devServer config. Added _redirects file for netlify deployment.

--- client/src/js/layouts/Home.js
+++ client/src/js/layouts/Home.js
@@ -4,6 +4,10 @@ import { Redirect } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
+      {/*
+    Temporary redirect to signin page.
+    Once backend auth is done, we'll redirect based on whether the user is signed in or not.
+    */}
       <Redirect to="/signin" />
     </div>
   );
